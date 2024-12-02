1c517b3a2b0a524d821c11442d7dcf33cfbcc8a6
wheelsbot7 <o.wertzberger@gmail.com>
2024-12-02 14:55:47 -0500

    visitor count

--- serve.ts
+++ serve.ts
@@ -1,5 +1,16 @@
 import Server from "lume/core/server.ts";
 
+const kv = await Deno.openKv();
+
+// increment a count using Deno KV
+await kv.atomic().sum(["visitors"], 1n).commit();
+
+// Get the latest visitor count
+const count = await kv.get(["visitors"]);
+
+console.log(`You are visitor number #${count.value}`);
+
+
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_site`,
@@ -8,3 +19,4 @@ const server = new Server({
 server.start();
 
 console.log("Listening on http://localhost:8000");
+
