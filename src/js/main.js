c5dd443227404dc98aa3b38ba874d6149e128bdb
wheelsbot7 <o.wertzberger@gmail.com>
2024-12-09 12:54:55 -0500

    finally fixed dark mode button

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,6 @@
 import Comments from "./comments.js";
-import LumeCode from "./lume_code.js";
-import LumeCopy from "./lume_copy.js";
+
 import SoftwareList from "./software-list.js";
 
 customElements.define("mastodon-comments", Comments);
 customElements.define("software-list", SoftwareList);
-customElements.define("lume-code", LumeCode);
-customElements.define("lume-copy", LumeCopy);
